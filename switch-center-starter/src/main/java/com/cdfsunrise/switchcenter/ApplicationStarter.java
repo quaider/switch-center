80d70f169c867a35bfb0cd0bb92ec0040442dd15
kratos zhang <pto.kratos@hotmail.com>
2023-03-18 16:27:08 +0800

    chage swagger to openapi3

--- switch-center-starter/src/main/java/com/cdfsunrise/switchcenter/ApplicationStarter.java
+++ switch-center-starter/src/main/java/com/cdfsunrise/switchcenter/ApplicationStarter.java
@@ -5,14 +5,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
 @EnableDiscoveryClient
 @MapperScan("com.cdfsunrise.switchcenter.adapter.driving.repository.*.dao")
-// @Import(SwaggerConfiguration.class)
 @EnableDomainInjector(basePackages = {"com.cdfsunrise.switchcenter.adapter.domain"})
 public class ApplicationStarter {
     public static void main(String[] args) {
